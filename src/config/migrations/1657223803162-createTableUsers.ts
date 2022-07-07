c380a937329c84d65934e25d812dd1fd9637d14a
hyuridev <hyuri.miranda.cortes@gmail.com>
2022-07-07 17:01:22 -0300

    feat: create migration users

---
+++ src/config/migrations/1657223803162-createTableUsers.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createTableUsers1657223803162 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "users",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                        isUnique: true,
+                        generationStrategy: "uuid",
+                    },
+                    {
+                        name: "name",
+                        type: "varchar",
+                    },
+                    {
+                        name: "email",
+                        type: "varchar",
+                    },
+                    {
+                        name: "password",
+                        type: "varchar",
+                    },
+                    {
+                        name: "status",
+                        type: "int",
+                    },
+                    {
+                        name: "createAt",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "updateAt",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            }),
+            true
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("users");
+    }
+}
